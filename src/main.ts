e236ad8fcc72ea9ed27c728a3ee4320263bf68c1
Bruno1224 <brunomenna2011@gmail.com>
2024-11-20 17:35:15 -0300

    dsaf

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,24 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAuth0} from '@auth0/auth0-angular';
+import { initializeApp } from "firebase/app";
+import { provideFirebaseApp } from '@angular/fire/app';
+const firebaseConfig = {
 
+  apiKey: "AIzaSyDlMmWY9vFEhB1sdKTRfNgpprBvrLoALq0",
 
+  authDomain: "artitectura-web.firebaseapp.com",
+
+  projectId: "artitectura-web",
+
+  storageBucket: "artitectura-web.firebasestorage.app",
+
+  messagingSenderId: "685712598634",
+
+  appId: "1:685712598634:web:6da02c385d9949c43fcc13"
+
+};
+const app = initializeApp(firebaseConfig)
 bootstrapApplication(AppComponent,{
   providers:[
     provideRouter(routes),
@@ -16,7 +32,8 @@ bootstrapApplication(AppComponent,{
         redirect_uri: window.location.origin
       }
     }),
-    provideHttpClient()
+    provideHttpClient(),
+    provideFirebaseApp(()=>initializeApp())
   ]
 }).catch(err=>console.error(err));
 
